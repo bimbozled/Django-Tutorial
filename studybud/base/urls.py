9f13a3b048a0535f8534268549f5d0c4956f06c5
bimbozled <abenbow1@charlotte.edu>
2025-02-14 13:18:04 -0500

    1:39:00 - CRUD

--- studybud/base/urls.py
+++ studybud/base/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 
-
-urlPatterns = [
+## this line below had a error for commits 2 and 3, fixed with chatgpt. 
+urlpatterns = [
     path('',views.home,name="home"),
     path('room/<str:pk>/', views.room, name="room"),
 
