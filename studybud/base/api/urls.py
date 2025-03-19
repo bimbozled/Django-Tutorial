b0035669769d3c1625dfc3b38b7118953f36f96b
bimbozled <abenbow1@charlotte.edu>
2025-03-19 09:48:27 -0400

    6:25:43 - Customizing User model

--- studybud/base/api/urls.py
+++ studybud/base/api/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',  views.getRoutes),
-    path('rooms/', views.getRooms),
-    path('rooms/<str:pk>/', views.getRoom),
+    path('', views.getRoutes),
+    path('rooms/',views.getRooms),
+    path('rooms/<str:pk>/',views.getRoom),
+
 ]
--- studybud/base/api/views.py
+++ studybud/base/api/views.py
@@ -2,18 +2,15 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from base.models import Room
 from .serializers import RoomSerializer
-from base.api import serializers
-
 
 @api_view(['GET'])
-def getRoutes(request):
-    routes = [
+def getRoute(request):
+    routes =[
         'GET /api',
         'GET /api/rooms',
         'GET /api/rooms/:id'
     ]
     return Response(routes)
-
 
 @api_view(['GET'])
 def getRooms(request):
@@ -23,7 +20,7 @@ def getRooms(request):
 
 
 @api_view(['GET'])
-def getRoom(request, pk):
-    room = Room.objects.get(id=pk)
+def getRoom(request,pk):
+    room = Room.objects.all(id=pk)
     serializer = RoomSerializer(room, many=False)
     return Response(serializer.data)
